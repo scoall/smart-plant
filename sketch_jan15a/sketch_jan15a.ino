0c6c36375ea195cad4b8f9ebd527e7d30b0845e5
nader2929 <nadersobhi1@hotmail.com>
2019-01-15 16:07:42 +0000

    Second term first commit

---
+++ sketch_jan15a/sketch_jan15a.ino
@@ -0,0 +1,21 @@
+
+
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 9
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(5000);
+}

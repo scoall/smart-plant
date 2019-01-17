e601c64460f96d21bff992317c80699721d985e5
nader2929 <nadersobhi1@hotmail.com>
2019-01-17 15:10:30 +0000

    Start of the website

---
+++ Code/view/index.php
@@ -0,0 +1,143 @@
+<?php
+
+ini_set('display_errors','On');
+error_reporting("Error 23");
+session_start();
+
+
+?>
+
+
+<html>
+    <head>
+        <title>Welcome to SmartEcoSystem !</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="Css.css">
+        <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Thasadith" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Caveat|Thasadith" rel="stylesheet"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+
+    <body>
+        <script>
+            $('this').ready(function(){
+
+
+                $(".image-cropper").eq(0).css({"width": "85px",  "height": "85px",  "position": "relative", "overflow": "hidden", "border-radius" : "50%", "margin-right": "25px", "box-shadow" : "5px 0px 8px black", "margin-top": "50px", "margin-left": "10px"});
+                $(".image-cropper").eq(1).css({"width": "100px",  "height": "100px",  "position": "relative", "overflow": "hidden", "border-radius" : "50%", "margin-right": "25px", "box-shadow" : "5px 0px 8px black", "margin-top": "20px"});
+                $(".image-cropper").eq(2).css({"width": "130px",  "height": "130px",  "position": "relative", "overflow": "hidden", "border-radius" : "50%", "margin-right": "25px", "box-shadow" : "5px 0px 8px black","opacity" : "1"});
+                $(".image-cropper").eq(3).css({"width": "100px",  "height": "100px",  "position": "relative", "overflow": "hidden", "border-radius" : "50%", "margin-right": "25px", "box-shadow" : "5px 0px 8px black", "margin-top": "20px"});
+                $(".image-cropper").eq(4).css({"width": "85px",  "height": "85px",  "position": "relative", "overflow": "hidden", "border-radius" : "50%", "margin-right": "10px", "box-shadow" : "5px 0px 8px black", "margin-top": "50px"});
+                $(".profile-pic").css({"margin-left": "-60%", "margin-top": "-25%"}); 
+
+
+
+
+
+
+
+
+            });
+
+        </script>
+        <div class="menu">
+            <nav class="navbar navbar-inner">
+                <div class="container text-center">
+                    <div class="row new2"><div class="col-lg-2"></div><div class="col-lg-8">
+
+                        <ul class="nav navbar-nav">
+
+                            <li><a href="">START</a></li>
+                            <li><a href="">ABOUT SMART ECO SYSTEM</a></li>
+                            <li><a href="">HOW IT WORKS</a></li>
+                            <li><a href="">ABOUT US</a></li>
+                            <li><a href="">CONTACT</a></li>
+
+
+                        </ul>
+                        </div><div class="col-lg-2"></div></div></div>
+            </nav>
+
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="new">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8">
+
+                        <p class="intro1" align="center">At Abertay Plant System we aim to supply you with the tools <br> for keeping your homes and gardens fresh and green. </p>
+                        <p class="intro2" align="center">Get ready for a whole range of smart devices <br> that make monitoring your plants as easy as pie.</p>
+                        <p class="intro3" align="center">New to gardening? Don’t worry! <br> We’ll help you colour your thumbs green in no time.</p>
+                    </div><div class="col-lg-2">
+
+                    <div class="social-container">
+                        <ul class="social-icons">
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li><br>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li><br>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li><br>
+                            <li><a href="#"><i class="fa fa-snapchat"></i></a></li><br>
+                        </ul>
+                        <!--
+<ul class="social-icons">
+<li><a href="#" class="social-square"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#" class="social-square"><i class="fa fa-dribbble"></i></a></li>
+<li><a href="#" class="social-square"><i class="fa fa-github"></i></a></li>
+<li><a href="#" class="social-square"><i class="fa fa-stack-overflow"></i></a></li>
+</ul>-->
+                    </div>
+
+
+                    </div>
+
+                </div>        
+
+            </div>
+
+            <div class="container">
+                <div class="row new2">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-1" align="center"><img src="images/leftarrow.png" alt="Avatar"  class="arrow" style="height:50px"></div>
+
+                    <div class="col-lg-6 center-block" id="izaismakingtrouble" align="center">
+
+                        <div class="container" align="center">
+                            <div class="row new2" align="center" id="here" >
+                                <div class="image-cropper col-lg-2"><img src="images/flower1.png" alt="Avatar" class="profile-pic" style="height:130px"></div>
+                                <div class="image-cropper col-lg-2"><img src="images/flower2.png" alt="Avatar" class="profile-pic" style="height:130px"></div>
+                                <div class="image-cropper col-lg-2"><img src="images/flower3.png" alt="Avatar" class="profile-pic" style="height:170px"></div>
+                                <div class="image-cropper col-lg-2"><img src="images/flower4.png" alt="Avatar" class="profile-pic" style="height:130px"></div>   
+                                <div class="image-cropper col-lg-2"><img src="images/flower5.png" alt="Avatar" class="profile-pic" style="height:130px"></div> 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-1" align="center"><img src="images/rightarrow.png" alt="Avatar" class="arrow"  style="height:50px"></div>
+                    <div class="col-lg-2"></div>
+
+
+
+
+                </div></div>
+
+
+        </div>
+
+
+        <footer>
+            <h5>© DNS Team 2019</h5>
+
+
+        </footer>
+
+
+
+
+    </body>
+</html>
+

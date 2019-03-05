481f522c0396ae3192a7fa0bb8d6933207a61cc4
nader2929 <nadersobhi1@hotmail.com>
2019-03-05 12:23:46 +0000

    no change trasnfer

--- Code/Device/uno/uno.ino
+++ Code/Device/uno/uno.ino
@@ -11,72 +11,73 @@ char chOut;
 char cr = 0x0D;
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // varia
-bool s1=true;
-bool s2, s3, s4=false;
+bool s1 = true;
+bool s2, s3, s4 = false;
 SoftwareSerial smirf(arduinoRXpin, arduinoTXpin);
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   Serial.println(F("Starting blueSmirf Tester." ) );
 
   pinMode(arduinoRXpin, INPUT);
   pinMode(arduinoTXpin, OUTPUT);
- 
+
 
   smirf.begin(9600);
   smirf.listen();
- 
+
 
 }
 
 
 
 
-void loop(){
-  if(s1==true){
-    int sensorValue = analogRead(A0);    
+void loop() {
+  if (s1 == true) {
+    int sensorValue = analogRead(A0);
     Serial.println("M");
-    Serial.println(sensorValue);
-    smirf.println("M"); 
-    smirf.println(400, DEC);
-    s1=false;
-    s2=true;
-    s3=false;
-    s4=false; 
+    Serial.println(400);
+    smirf.print('M');
+    smirf.print(400, DEC);
+    s1 = false;
+    s2 = true;
+    s3 = false;
+    s4 = false;
   }
-  else if(s2==true){
-    int sensorValue = analogRead(A0);    
+  else if (s2 == true) {
+    int sensorValue = analogRead(A0);
     Serial.println("H");
-    Serial.println(sensorValue);
-    smirf.println("H"); 
-    smirf.println(300, DEC);
-    s1=false;
-    s2=false;
-    s3=true;
-    s4=false; 
+    Serial.println(300);
+    smirf.print('H');
+    smirf.print(300, DEC);
+    s1 = false;
+    s2 = false;
+    s3 = true;
+    s4 = false;
   }
-  else if(s3==true){
-    int sensorValue = analogRead(A0);    
+  else if (s3 == true) {
+    int sensorValue = analogRead(A0);
     Serial.println("T");
-    Serial.println(sensorValue);
-    smirf.println("T"); 
-    smirf.println(200, DEC);
-    s1=false;
-    s2=false;
-    s3=false;
-    s4=true; 
+    Serial.println(200);
+    smirf.print('T');
+    smirf.print(200, DEC);
+    s1 = false;
+    s2 = false;
+    s3 = false;
+    s4 = true;
   }
-  else if(s4==true){
-    int sensorValue = analogRead(A0);    
+  else if (s4 == true) {
+    int sensorValue = analogRead(A0);
     Serial.println("L");
-    Serial.println(sensorValue);
-    smirf.println("L"); 
-    smirf.println(100, DEC);
-    s1=true;
-    s2=false;
-    s3=false;
-    s4=false; 
+    Serial.println(100);
+    smirf.print('L');
+    smirf.print(100, DEC);
+    s1 = true;
+    s2 = false;
+    s3 = false;
+    s4 = false;
+   // smirf.print("NAder and aScott")
   }
 
-delay(1000);
+  delay(1000);
 }
--- Code/Device/wemos/wemos.ino
+++ Code/Device/wemos/wemos.ino
@@ -9,75 +9,123 @@ In this sketch serial commands are passed from a terminal program to the SM5100B
 An activated SIM card must be inserted into the SIM card holder on the board in order to use the device!
 
 */
+#include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>  //Include the NewSoftSerial library to send serial commands to the cellular module.
 #include <string.h>         //Used for string manipulations
+#include <ArduinoJson.h>
 
 char incoming_char = 0;      //Will hold the incoming character from the Serial Port.
-
+//StaticJsonDocument<1024> jsonBuffer;
+DynamicJsonDocument jsonBuffer(1024);
 SoftwareSerial cell(D2,D3);  //Create a 'fake' serial port. Pin 2 is the Rx pin, pin 3 is the Tx pin.
 bool moisture, humidity, light, temp = false;
-char m,h,l,t;
+char m='X'; char h='X'; char l='X'; char t='X';
+char mvalue=0; char hvalue=0; char lvalue=0; char tvalue=0;
+int mvalue1=0; int hvalue1=0; int lvalue1=0; int tvalue1=0;
+
+int count = 0;
+char jsonstr;
 void setup()
 {
   //Initialize serial ports for communication.
   Serial.begin(9600);
   cell.begin(9600);
-  
+  Serial.print(WiFi.macAddress());
   //Let's get started!
   Serial.println("Starting SM5100B Communication...");
 }
 
 
 void loop() {
-  
+  char check;
+  char value;
   //If a character comes in from the cellular module...
-  if(cell.available() > 0)
+  if(cell.available())
   {
-    
+
     incoming_char=cell.read();
     if (incoming_char =='M'){
-      Serial.print("Moisture");
+      Serial.println();
+      count=count+1;
+      m='M';
+      check = incoming_char;
       moisture=true;
-      humidity, light, temp = false;
+      humidity=false; light=false; temp = false;
     }
     else if (incoming_char =='H'){
-      Serial.print("Humidity");
+      Serial.println();
+      count=count+1;
+      h='H';
+      check = incoming_char;
       humidity=true;
-      moisture, light, temp = false;
+      moisture=false; light=false; temp = false;
     }
     else if (incoming_char =='T'){
-      Serial.print("Temp");
+      Serial.println();
+      count=count+1;
+      t='T';
+      check = incoming_char;
       temp=true;
-      moisture, light, humidity = false;
+      moisture=false; light=false; humidity = false;
     }
     else if (incoming_char =='L'){
-      Serial.print("Light");
+      Serial.println();
+      count=count+1;
+      l='L';
+      check = incoming_char;
       light=true;
-      moisture, humidity, temp = false;
+      moisture=false; humidity=false; temp = false;
     }
-    else{
-      char value = incoming_char;
-      Serial.print(value);
+    
+    else{    
+      value = value+incoming_char;
+      Serial.print(value);       
+      if (moisture==true){
+        mvalue=value;
+        mvalue1=mvalue1+1;
+      }
+      else if (humidity==true){
+        hvalue=value;
+        hvalue1=hvalue1+1;
+      }
+      else if (light==true){
+        lvalue=value;
+        lvalue1=lvalue1+1;
+      }
+      else if (temp==true){
+        tvalue=value;
+        tvalue1=tvalue1+1;
+      }       
+    }
+    if (mvalue1>=6 && tvalue1>=6 && lvalue1>=6 && hvalue1>=6){
+    
         
-      /*if (moisture==true){Serial.println("M");}
-      else if (humidity==true){Serial.println("H");}
-      else if (light==true){Serial.println("L");}
-      else if (temp==true){Serial.println("T");}
-      /*Serial.print("M is: ");
-      Serial.print(m);
-      Serial.print("H is: ");
-      Serial.print(h);
-      Serial.print("L is: ");
-      Serial.print(l);
-      Serial.print("T is: ");
-      Serial.print(t);*/
+        jsonBuffer["m"]=mvalue;
+        jsonBuffer["h"]=hvalue;
+        jsonBuffer["t"]=tvalue;
+        jsonBuffer["l"]=lvalue;
+        /*Serial.println(mvalue);
+        Serial.println(hvalue);
+        Serial.println(tvalue);
+        Serial.println(lvalue);*/
+        serializeJson(jsonBuffer, Serial);
+        mvalue1=0;
+        hvalue1=0;
+        tvalue1=0;
+        lvalue1=0;
+        mvalue=0;
+        hvalue=0;
+        tvalue=0;
+        lvalue=0;
     }
+    
   }
   //If a character is coming from the terminal to the Arduino...
   if(Serial.available() >0)
   {
     incoming_char=Serial.read();  //Get the character coming from the terminal
     cell.print(incoming_char);    //Send the character to the cellular module.
+    
   }
 }
 

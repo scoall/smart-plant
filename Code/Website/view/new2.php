1f751d562d446275f3cf03a5840925fa7f21e6dd
Missmevas <43172697+Missmevas@users.noreply.github.com>
2019-03-13 11:09:44 +0000

    Add files via upload

--- Code/Website/view/new2.php
+++ Code/Website/view/new2.php
@@ -3,7 +3,7 @@ ini_set('display_errors','On');
 error_reporting(E_ALL);
 session_start();
 $_POST['username'] = "Missmeva"; //CHANGE IT !!!
- ?>
+?>
 
 
 
@@ -28,11 +28,20 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
 
 
         <style>
+
+            .form-control:focus {
+                border-color: black;
+
+            }
+
+           
+
             .ct-label
             {
                 color: #fff !important;
                 font-size:10px;
             }
+
             .ct-grids
             {
                 color: #fff !important;
@@ -77,8 +86,8 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
                 margin: 0;
             }
             .ct-legend .ct-series-0:before {
-                background-color: #d70206;
-                border-color: #d70206;
+                background-color: aquamarine;
+                border-color: aquamarine;
             }
             .ct-legend .ct-series-1:before {
                 background-color: #f05b4f;
@@ -143,6 +152,11 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
 
             .crazyPink ~ svg .ct-series-c .ct-line, .crazyPink ~ svg .ct-series-c .ct-point {
                 stroke: #F06292;
+            }
+
+            .ct-series-a .ct-line,
+            .ct-series-a .ct-point {
+                stroke: aquamarine;
             }
             /* Page styling */
 
@@ -274,9 +288,9 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
                         <center><h4 class="modal-title">Add another plant to your unique collection</h4></center>
                     </div>
                     <div class="modal-body"> 
-                        
+
                         <center><img src="planticon.png" height="100" width="100" style="display:block"></center><br>
-                        
+
                         <form action="../model/addPlant.php" method="post" name="formm" id="formm" enctype="multipart/form-data">
                             <input type="hidden" name="username" value="Missmeva">
                             <center><div class="form-group">
@@ -375,49 +389,49 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
                             </tr>
                         </thead>
                         <tbody> 
-                            
+
                             <?php include '../model/getPlants.php'; ?>
-                            
+
                             <!-- ALL THIS NEEDS TO BE CHANGED/DISPLAYED WITH PHP+HTML -->
                             <!--
-                            <tr>
-                                <th><img src="flower3.png" class="img-circle" height="80" width="80" style="display:block"></th>
-                                <td>Veratrum viride</td>
-                                <th><center><img src="water.png"  height="50" width="50" style="display:block">Water me !</center></th>
-                                <td>300</td>
-                                <td>18.2°</td>
-                                <td>80%</td>
-                                <td>250</td>
-                                <td><button type="button" class="btn btn-info" height="60" width="60">MORE</button></td>
-                            </tr>
-                            <tr>
-                                <th><img src="flower1.png" class="img-circle" height="80" width="80" style="display:block"></th>
-                                <td>Betula pendula</td>
-                                <th><center><img src="like.png"  height="40" width="40" style="display:block;margin-top:10px; margin-bottom:10px">Perfect</center></th>
+<tr>
+<th><img src="flower3.png" class="img-circle" height="80" width="80" style="display:block"></th>
+<td>Veratrum viride</td>
+<th><center><img src="water.png"  height="50" width="50" style="display:block">Water me !</center></th>
+<td>300</td>
+<td>18.2°</td>
+<td>80%</td>
+<td>250</td>
+<td><button type="button" class="btn btn-info" height="60" width="60">MORE</button></td>
+</tr>
+<tr>
+<th><img src="flower1.png" class="img-circle" height="80" width="80" style="display:block"></th>
+<td>Betula pendula</td>
+<th><center><img src="like.png"  height="40" width="40" style="display:block;margin-top:10px; margin-bottom:10px">Perfect</center></th>
 
-                                <td>600</td>
-                                <td>17.5°</td>
-                                <td>65%</td>
-                                <td>320</td>
-                                <td><button type="button" class="btn btn-info" height="60" width="60">MORE</button></td>
+<td>600</td>
+<td>17.5°</td>
+<td>65%</td>
+<td>320</td>
+<td><button type="button" class="btn btn-info" height="60" width="60">MORE</button></td>
 
-                            </tr>
-                            <tr>
-                                <th><img src="flower2.png" class="img-circle" height="80" width="80" style="display:block"></th>
-                                <td>Solanum dulcamara</td>
-                                <th><center><img src="temperature.png" class="img-circle" height="50" width="50" style="display:block">Too warm</center></th>
+</tr>
+<tr>
+<th><img src="flower2.png" class="img-circle" height="80" width="80" style="display:block"></th>
+<td>Solanum dulcamara</td>
+<th><center><img src="temperature.png" class="img-circle" height="50" width="50" style="display:block">Too warm</center></th>
 
-                                <td>190</td>
-                                <td>20.3°</td>
-                                <td>78%</td>
-                                <td>700</td>
-                                <td><button type="button" class="btn btn-info" height="60" width="60">MORE</button></td>
-                            </tr> -->
-                            
-                            
-                            
-                            
-                            
+<td>190</td>
+<td>20.3°</td>
+<td>78%</td>
+<td>700</td>
+<td><button type="button" class="btn btn-info" height="60" width="60">MORE</button></td>
+</tr> -->
+
+
+
+
+
                         </tbody> <!-- ALL THIS NEEDS TO BE CHANGED/DISPLAYED WITH PHP+HTML -->
                     </table>
 
@@ -428,31 +442,61 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
                 <section class="row dark" id="sec01">
                     <div class="content">
                         <h1 >Moisture readings</h1>
-                        <div id="legends">
+                        <div id="legends1">
 
                         </div>
-                        <div class="ct-chart ct-perfect-fourth"  ></div>
+                        <div class="ct-chart ct-perfect-fourth" id="chart1"></div>
+                        <center><select class="form-control" id="sel1" style="width:140px; background-color: grey; color:white;">
+                            <option>Day View</option>
+                            <option>Week View</option>
+                            <option>Month View</option>
+                            </select></center>
                     </div>
                 </section><!-- END row -->
 
                 <section class="row" id="sec02">
                     <div class="content">
                         <h1>Temperature readings</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, recusandae, at, labore velit eligendi amet nobis repellat natus sequi sint consectetur excepturi doloribus vero provident consequuntur accusamus quisquam nesciunt cupiditate soluta alias illo et deleniti voluptates facilis repudiandae similique dolore quaerat architecto perspiciatis officiis dolor ullam expedita suscipit neque minima rem praesentium inventore ab officia quos dignissimos esse quam placeat iste porro eius! Minus, aspernatur nesciunt </p>
+                        <div id="legends2">
+
+                        </div>
+                        <div class="ct-chart ct-perfect-fourth" id="chart2" ></div>
+                        <center><select class="form-control" id="sel1" style="width:140px;">
+                            <option>Day View</option>
+                            <option>Week View</option>
+                            <option>Month View</option>
+                            </select></center>
+
                     </div>
                 </section><!-- END row -->
 
                 <section class="row dark" id="sec03">
                     <div class="content">
                         <h1>Light level</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, recusandae, at, labore velit eligendi amet nobis repellat natus sequi sint consectetur excepturi doloribus vero provident consequuntur accusamus quisquam nesciunt cupiditate soluta alias illo et deleniti voluptates facilis repudiandae similique dolore quaerat architecto perspiciatis officiis dolor ullam expedita suscipit neque minima rem praesentium inventore ab officia quos dignissimos esse quam placeat iste porro eius! Minus, aspernatur nesciunt consectetur. Sit, eius, itaque, porro, beatae impedit officia tenetur reiciendis autem vitae a quae ipsam repudiandae odio dolorum </p>
+                        <div id="legends3">
+
+                        </div>
+                        <div class="ct-chart ct-perfect-fourth" id="chart3" ></div>
+                        <center><select class="form-control" id="sel1" style="width:140px; background-color: grey; color:white;">
+                            <option>Day View</option>
+                            <option>Week View</option>
+                            <option>Month View</option>
+                            </select></center>
                     </div>
                 </section><!-- END row -->
 
                 <section class="row" id="sec04">
                     <div class="content">
                         <h1>Humidity</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, recusandae, at, labore velit eligendi amet nobis repellat natus sequi sint consectetur excepturi doloribus vero provident consequuntur accusamus quisquam nesciunt cupiditate soluta alias illo et deleniti voluptates facilis repudiandae similique dolore quaerat architecto perspiciatis officiis dolor ullam expedita suscipit neque minima rem praesentium inventore ab officia quos dignissimos esse quam placeat iste porro eius! Minus, aspernatur nesciunt consectetur. Sit, eius, itaque, porro, beatae impedit officia tenetur reiciendis autem vitae a quae ipsam repudiandae odio dolorum </p>
+                        <div id="legends4">
+
+                        </div>
+                        <div class="ct-chart ct-perfect-fourth" id="chart4" ></div>
+                        <center><select class="form-control" id="sel1" style="width:140px;">
+                            <option>Day View</option>
+                            <option>Week View</option>
+                            <option>Month View</option>
+                            </select></center>
                     </div>
                 </section><!-- END row -->
             </div>
@@ -538,149 +582,15 @@ $_POST['username'] = "Missmeva"; //CHANGE IT !!!
             });
 
         </script>
-        <script>
 
+        <?php include '../model/getDataMoisture.php'; ?>
 
-            var someDiv = document.getElementById('legends');
-            var chart = new Chartist.Line('.ct-chart', {
-                labels: ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19'],
-                series: [{"name": "2018-12-02", "data":[30.2,30.3,30.2,30.2]},{"name": "2018-12-04", "data":[30.3,30.3,30.2]},{"name": "2018-12-05", "data":[30.3,30.2,30.5,30.4,30.5]},{"name": "2018-12-06", "data":[30.4,30.4]},{"name": "2018-12-07", "data":[30.4,30.4,30.4,30.4]},{"name": "2018-12-08", "data":[30.4,30.4,30.4,30.4,30.3,30.4,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.2,30.2,30.2,30.2]},]
+        <!--
 
-            }, {
+labels: ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19'],
+series: [{"name": "2018-12-02", "data":[30.2,30.3,30.2,30.2]},{"name": "2018-12-04", "data":[30.3,30.3,30.2]},{"name": "2018-12-05", "data":[30.3,30.2,30.5,30.4,30.5]},{"name": "2018-12-06", "data":[30.4,30.4]},{"name": "2018-12-07", "data":[30.4,30.4,30.4,30.4]},{"name": "2018-12-08", "data":[30.4,30.4,30.4,30.4,30.3,30.4,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.3,30.2,30.2,30.2,30.2]},]
 
-                fullWidth: true,
-                chartPadding: {
-                    right: 40
-                },
-                plugins: [
-                    Chartist.plugins.legend({
-                        clickable: true,
-                        position: someDiv
-                    })
-                ]
-            });
+-->
 
-
-
-            // Let's put a sequence number aside so we can use it in the event callbacks
-            var seq = 0,
-                delays = 80,
-                durations = 500;
-
-            // Once the chart is fully created we reset the sequence
-            chart.on('created', function() {
-                seq = 0;
-            });
-
-            // On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
-            chart.on('draw', function(data) {
-                seq++;
-
-                if(data.type === 'line') {
-                    // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
-                    data.element.animate({
-                        opacity: {
-                            // The delay when we like to start the animation
-                            begin: seq * delays + 1000,
-                            // Duration of the animation
-                            dur: durations,
-                            // The value where the animation should start
-                            from: 0,
-                            // The value where it should end
-                            to: 1
-                        }
-                    });
-                } else if(data.type === 'label' && data.axis === 'x') {
-                    data.element.animate({
-                        y: {
-                            begin: seq * delays,
-                            dur: durations,
-                            from: data.y + 100,
-                            to: data.y,
-                            // We can specify an easing function from Chartist.Svg.Easing
-                            easing: 'easeOutQuart'
-                        }
-                    });
-                } else if(data.type === 'label' && data.axis === 'y') {
-                    data.element.animate({
-                        x: {
-                            begin: seq * delays,
-                            dur: durations,
-                            from: data.x - 100,
-                            to: data.x,
-                            easing: 'easeOutQuart'
-                        }
-                    });
-                } else if(data.type === 'point') {
-                    data.element.animate({
-                        x1: {
-                            begin: seq * delays,
-                            dur: durations,
-                            from: data.x - 10,
-                            to: data.x,
-                            easing: 'easeOutQuart'
-                        },
-                        x2: {
-                            begin: seq * delays,
-                            dur: durations,
-                            from: data.x - 10,
-                            to: data.x,
-                            easing: 'easeOutQuart'
-                        },
-                        opacity: {
-                            begin: seq * delays,
-                            dur: durations,
-                            from: 0,
-                            to: 1,
-                            easing: 'easeOutQuart'
-                        }
-                    });
-                } else if(data.type === 'grid') {
-                    // Using data.axis we get x or y which we can use to construct our animation definition objects
-                    var pos1Animation = {
-                        begin: seq * delays,
-                        dur: durations,
-                        from: data[data.axis.units.pos + '1'] - 30,
-                        to: data[data.axis.units.pos + '1'],
-                        easing: 'easeOutQuart'
-                    };
-
-                    var pos2Animation = {
-                        begin: seq * delays,
-                        dur: durations,
-                        from: data[data.axis.units.pos + '2'] - 100,
-                        to: data[data.axis.units.pos + '2'],
-                        easing: 'easeOutQuart'
-                    };
-
-                    var animations = {};
-                    animations[data.axis.units.pos + '1'] = pos1Animation;
-                    animations[data.axis.units.pos + '2'] = pos2Animation;
-                    animations['opacity'] = {
-                        begin: seq * delays,
-                        dur: durations,
-                        from: 0,
-                        to: 1,
-                        easing: 'easeOutQuart'
-                    };
-
-                    data.element.animate(animations);
-                }
-            });
-
-
-
-
-
-            chart.on('created', function() {
-                if(window.__exampleAnimateTimeout) {
-                    clearTimeout(window.__exampleAnimateTimeout);
-                    window.__exampleAnimateTimeout = null;
-                }
-                window.__exampleAnimateTimeout = setTimeout(chart.update.bind(chart), 100000);
-            });
-
-
-        </script>
-     
     </body>
+</html>
